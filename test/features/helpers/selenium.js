1b0ad4f92a371dd6e36b16bc564a1694d4b27aa9
seanstrom <sean@seanhagstrom.com>
2015-09-20 20:36:58 -0700

    add selenium-standalone wrapper
    create a wrapper around selenium-standalone that supports promises.

---
+++ test/features/helpers/selenium.js
@@ -0,0 +1,20 @@
+var Promise = require('es6-promise')
+var selenium = require('selenium-standalone')
+
+exports.install = function (opts) {
+  return new Promise(function (resolve, reject) {
+    selenium.install(opts, function (err) {
+      if (err) { return reject(err) }
+      return resolve()
+    })
+  })
+}
+
+exports.start = function (opts) {
+  return new Promise(function (resolve, reject) {
+    selenium.start(opts, function (err, child) {
+      if (err) { return reject(err) }
+      return resolve(child)
+    })
+  })
+}
